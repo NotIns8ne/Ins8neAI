5cab8cd7056c2c4921dd90eafda76b057c9756ed
NotIns8ne <c63513389@gmail.com>
2024-11-21 15:16:24 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Create and work with classes
+class Solver:
+    def solve(self, args, data):
+        #solve forwards
+        #create basis
+            #save basis while resolving
+        #
+        
+        return 'Solving ' + args + data
+        
+print(Solver().solve('Amber'))
+
+brary
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+# Create and work with classes
+class Greeting:
+    def greet(self, name):
+        return 'Hello ' + name
+
+print(Greeting().greet('Amber'))
+
+# Have fun coding in Python!
